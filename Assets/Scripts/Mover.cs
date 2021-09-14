018832ab34084e1f511bcd17c063ccbde9930106
Madasski <vansemen@mail.ru>
2021-09-14 18:57:39 +0300

    add basic movement

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Mover
+{
+    private Character _character;
+
+    public Mover(Character character)
+    {
+        _character = character;
+    }
+
+    public void Move(Vector2 moveInput)
+    {
+        var targetPosition = _character.Rigidbody.position + new Vector3(moveInput.x, 0, moveInput.y) * Time.deltaTime * _character.MovementSettings.Speed;
+        _character.Rigidbody.MovePosition(targetPosition);
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerInput
+{
+    private Vector2 _movementInput;
+
+    public Vector2 MovementInput
+    {
+        get
+        {
+            if (_movementInput.magnitude > 1)
+            {
+                _movementInput.Normalize();
+            }
+
+            return _movementInput;
+        }
+    }
+
+    public void Read()
+    {
+        _movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+}
