21fae44a2effbfe2efc89cdb581b2adaebb41240
Madasski <vansemen@mail.ru>
2021-09-15 16:33:53 +0300

    add basic rotation behavior

---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LookAtMouse : LookAtBase
+{
+    protected override Vector3? GetDirectionToLook()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out var hit))
+        {
+            var directionToLook = hit.point - transform.position;
+            return directionToLook;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
