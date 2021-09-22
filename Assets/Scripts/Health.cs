5afa673568df86747a5ab42c1ebea6d095a443a5
Madasski <vansemen@mail.ru>
2021-09-22 21:28:50 +0300

    add basic health, damage behavior

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour, IDamageable
+{
+    public int MaxHealth;
+
+    private int _currentHealth;
+
+    private void Awake()
+    {
+        _currentHealth = MaxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth <= 0)
+            Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -2,9 +2,10 @@
 
 namespace Game.Weapons
 {
-    public class Projectile: MonoBehaviour
+    public class Projectile : MonoBehaviour
     {
         public float Speed;
+        public int DamageOnHit;
 
         private Rigidbody _rigidbody;
 
@@ -15,10 +16,22 @@ namespace Game.Weapons
 
         private void FixedUpdate()
         {
-            // transform.position += transform.forward*Time.deltaTime*Speed;
             var targetPosition = _rigidbody.position + transform.forward * Time.deltaTime * Speed;
             _rigidbody.MovePosition(targetPosition);
         }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Damage(other);
+            Destroy(gameObject);
+        }
+
+        private void Damage(Collider other)
+        {
+            if (other.gameObject.TryGetComponent<IDamageable>(out var damageable))
+            {
+                damageable.TakeDamage(DamageOnHit);
+            }
+        }
     }
 }
-
