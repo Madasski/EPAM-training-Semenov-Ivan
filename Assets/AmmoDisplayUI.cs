b3ebb542ad91847e076217ce22de3fb16db85f28
Madasski <vansemen@mail.ru>
2021-09-29 13:34:47 +0300

    add weapon icons, move scripts

---
+++ Assets/AmmoDisplayUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoDisplayUI : MonoBehaviour
+{
+    public Image WeaponIcon;
+    public Text AmmoLeftText;
+
+    public void UpdateAmmoCounter(int ammoLeft)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/UI/HUD/HUD.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    public PlayerCharacter PlayerCharacter;
+    public AmmoDisplayUI AmmoDisplayUI;
+
+    private void OnEnable()
+    {
+        PlayerCharacter.
+    }
+
+    private void OnDisable()
+    {
+    }
+}
