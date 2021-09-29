ba3fff9c697425426e0a2b1a6ed9b0f9732c2296
Madasski <vansemen@mail.ru>
2021-09-29 13:36:07 +0300

    delete not used WeaponData.cs

--- Assets/Scripts/Weapons/WeaponData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace Game.Weapons
-{
-    [CreateAssetMenu(fileName = "WeaponData", menuName = "ScriptableObjects/WeaponData", order = 0)]
-    public class WeaponData : ScriptableObject
-    {
-    }
-}
