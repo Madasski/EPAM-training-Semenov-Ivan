6ac1529d7b502d61e6cf2e9ae046d033f096add7
Madasski <vansemen@mail.ru>
2021-09-30 13:27:12 +0300

    add back sprite slow change for healthbar

--- Assets/Scripts/UI/HealthBarUI.cs
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBarUI : MonoBehaviour
@@ -7,8 +8,13 @@ public class HealthBarUI : MonoBehaviour
     public Image MiddleSprite;
     public Image RightSprite;
 
+    public Image BackLeftSprite;
+    public Image BackMiddleSprite;
+    public Image BackRightSprite;
+
     private Health _target;
     private RectTransform _transform;
+    private Coroutine backSpriteChangingCoroutine;
 
     private void Awake()
     {
@@ -17,6 +23,13 @@ public class HealthBarUI : MonoBehaviour
 
     private void Update()
     {
+        FollowTarget();
+    }
+
+    private void FollowTarget()
+    {
+        if (!_target) return;
+
         var targetPos = Camera.main.WorldToScreenPoint(_target.transform.position + Vector3.forward * .5f);
         _transform.position = targetPos;
     }
@@ -25,8 +38,7 @@ public class HealthBarUI : MonoBehaviour
     {
         _target = targetHealth;
         _target.OnHealthChange += ChangeHealth;
-        var targetPos = Camera.main.WorldToScreenPoint(_target.transform.position + Vector3.forward * .5f);
-        _transform.position = targetPos;
+        FollowTarget();
     }
 
     private void OnDisable()
@@ -41,8 +53,30 @@ public class HealthBarUI : MonoBehaviour
     {
         var percent = (float) newHealth / (float) maxHealth * 3f;
 
-        RightSprite.fillAmount = percent-2f;
-        MiddleSprite.fillAmount = percent-1f;
+        RightSprite.fillAmount = percent - 2f;
+        MiddleSprite.fillAmount = percent - 1f;
         LeftSprite.fillAmount = percent;
+
+        if (backSpriteChangingCoroutine != null) StopCoroutine(backSpriteChangingCoroutine);
+        
+        backSpriteChangingCoroutine = StartCoroutine(BackSpriteChanging(percent));
+    }
+
+    private IEnumerator BackSpriteChanging(float targetPercent)
+    {
+        float percentToDraw;
+        float currentPercent = -1f;
+
+        while (!Mathf.Approximately(targetPercent, currentPercent))
+        {
+            currentPercent = BackRightSprite.fillAmount + BackMiddleSprite.fillAmount + BackLeftSprite.fillAmount;
+            percentToDraw = Mathf.Lerp(currentPercent, targetPercent, .05f);
+            
+            BackRightSprite.fillAmount = percentToDraw - 2f;
+            BackMiddleSprite.fillAmount = percentToDraw - 1f;
+            BackLeftSprite.fillAmount = percentToDraw;
+
+            yield return new WaitForSeconds(.1f);
+        }
     }
 }
